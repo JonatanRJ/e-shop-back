00e0cb33ffa83561903a73bb10c39940e8064a85
jonatanrj <jonatan.ramirez5280@alumnos.udg.mx>
2022-09-11 18:52:26 -0500

    Swagger documentation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const { keepAlive } = require('./db/query');
 port = process.env.PORT || 3000;
 
@@ -13,8 +14,14 @@ setInterval(keepAlive, 1000);
 //Middleware
 app.use( cors() );
 
+//Docs
+const swaggerDocument = require('./openapi.json');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 //Server
 app.use('/api/products/', require('./routes/products'))
+
+
 
 //Start
 app.listen(port, () => {
