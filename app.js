17d0f22f48dd25256e29bd44951ab89376eabccf
jonatanrj <jonatan.ramirez5280@alumnos.udg.mx>
2022-09-11 19:08:24 -0500

    minor fices

--- app.js
+++ app.js
@@ -7,21 +7,18 @@ const swaggerUi = require('swagger-ui-express');
 const { keepAlive } = require('./db/query');
 port = process.env.PORT || 3000;
 
-
 //DB
 setInterval(keepAlive, 1000);
 
 //Middleware
 app.use( cors() );
 
-//Docs
+//Swagger documentation
 const swaggerDocument = require('./openapi.json');
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //Server
 app.use('/api/products/', require('./routes/products'))
-
-
 
 //Start
 app.listen(port, () => {
